e6cbc47a08caa0f6379201391bd16c290bbe7812
thexyman <makdaa1@gmail.com>
2018-09-19 12:59:52 +0100

    Done.

---
+++ models/puppy.rb
@@ -0,0 +1,17 @@
+class Puppy
+
+  attr_accessor :name, :breed, :age
+
+  @@all = []
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
+
+  def self.all
+    @@all
+  end
+
+end
